14bcfc7ae6279ffa433e285553f27c4d0e62d93b
Csordás Márton <csmarton@caesar.elte.hu>
2016-07-20 11:17:11 +0200

    Metrics model

---
+++ plugins/metrics/model/include/model/metrics.h
@@ -0,0 +1,53 @@
+#ifndef CC_MODEL_METRICS_H
+#define CC_MODEL_METRICS_H
+
+#include <string>
+
+#include <odb/core.hxx>
+#include <odb/lazy-ptr.hxx>
+#include <odb/nullable.hxx>
+
+#include <model/file.h>
+
+namespace cc
+{
+namespace model
+{
+
+#pragma db object
+struct Metrics
+{
+  enum Type
+  {
+    ORIGINAL_LOC = 1,
+    NONBLANK_LOC = 2,
+    CODE_LOC = 3,
+    MCCABE = 4
+  };
+
+  #pragma db id auto
+  uint64_t id;
+
+  #pragma db not_null
+  FileId file;
+
+  #pragma db not_null
+  unsigned metric;
+  
+  #pragma db not_null
+  Type type;
+
+  friend class odb::access;
+};
+
+#pragma db view object(Metrics)
+struct MetricsCount
+{
+  #pragma db column("count(" + Metrics::id + ")")
+  std::size_t count;
+};
+
+} //model
+} //cc
+
+#endif // CC_MODEL_METRICS_H
