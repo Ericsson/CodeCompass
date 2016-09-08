6618caa6f8505952db349a90c342de8e31f182ea
Csordás Márton <csmarton@caesar.elte.hu>
2016-09-08 10:53:41 +0200

    Git parser

---
+++ plugins/git/parser/include/gitparser/gitparser.h
@@ -0,0 +1,27 @@
+#ifndef CC_PARSER_GITPARSER_H
+#define CC_PARSER_GITPARSER_H
+
+#include <parser/abstractparser.h>
+#include <parser/parsercontext.h>
+
+#include <util/parserutil.h>
+
+namespace cc
+{
+namespace parser
+{
+
+class GitParser : public AbstractParser
+{
+public:
+  GitParser(ParserContext& ctx_);
+  virtual std::vector<std::string> getDependentParsers() const override;
+  virtual bool parse() override;
+private:
+  util::DirIterCallback getParserCallback();
+};
+
+} // parser
+} // cc
+
+#endif //CC_PARSER_GITPARSER_H
