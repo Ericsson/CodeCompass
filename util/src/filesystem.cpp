762ee5aeff7458761b3f91d6c610c8a040a0f3fc
Anett Fekete <anett.fekete@ericsson.com>
2019-11-05 13:43:45 +0100

    Webserver path is searched for in PATH if not called from its install directory.

--- util/src/filesystem.cpp
+++ util/src/filesystem.cpp
@@ -1,6 +1,9 @@
 #include <boost/filesystem.hpp>
 
 #include <util/filesystem.h>
+#include <util/logutil.h>
+
+#include <stdlib.h>
 
 namespace fs = boost::filesystem;
 
@@ -15,8 +18,34 @@ std::string binaryPathToInstallDir(const char* path)
   // of any symbolic links and relative references.
   // The path is then parented twice (from binary to "bin" folder, and then
   // the root).
-  return fs::canonical(fs::system_complete(fs::path(path)))
-    .parent_path().parent_path().string();
+  // If the path does not exist, the given binary is searched for in the PATH
+  // environment variable.
+
+  if (fs::exists(fs::system_complete(fs::path(path))))
+  {
+    return fs::canonical(fs::system_complete(fs::path(path)))
+      .parent_path().parent_path().string();
+  }
+  else
+  {
+    std::string webserverPath;
+    std::string fullPath = std::getenv("PATH");
+    std::string delimiter = ":";
+
+    size_t pos = 0;
+    while ((pos = fullPath.find(delimiter)) != std::string::npos)
+    {
+      webserverPath = fullPath.substr(0, pos) + "/" + path;
+
+      if (fs::exists(fs::path(webserverPath)))
+      {
+        return fs::canonical(fs::path(webserverPath))
+          .parent_path().parent_path().string();
+      }
+
+      fullPath.erase(0, pos + delimiter.length());
+    }
+  }
 }
 
 } // namespace util
