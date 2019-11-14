7645f505885594bdb5733302069ad4d20ab6cd57
Anett Fekete <anett.fekete@ericsson.com>
2019-11-14 16:46:50 +0100

    Error when running CodeCompass binary from extended PATH (#363)
    Webserver path is searched for in PATH if not called from its install directory.

--- util/src/filesystem.cpp
+++ util/src/filesystem.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+
 #include <boost/filesystem.hpp>
 
 #include <util/filesystem.h>
@@ -15,8 +17,37 @@ std::string binaryPathToInstallDir(const char* path)
   // of any symbolic links and relative references.
   // The path is then parented twice (from binary to "bin" folder, and then
   // the root).
-  return fs::canonical(fs::system_complete(fs::path(path)))
-    .parent_path().parent_path().string();
+  // If the path does not exist, the given binary is searched for in the PATH
+  // environment variable.
+  // TODO: replace else branch with fs::search_path if Boost 1.64 or above is used.
+
+  if (fs::exists(fs::system_complete(fs::path(path))))
+  {
+    return fs::canonical(fs::system_complete(fs::path(path)))
+      .parent_path().parent_path().string();
+  }
+  else
+  {
+    fs::path pPath;
+    std::string fullPath = std::getenv("PATH");
+    std::string delimiter = ":";
+
+    std::size_t pos = 0;
+    while ((pos = fullPath.find(delimiter)) != std::string::npos)
+    {
+      pPath = fs::path(fullPath.substr(0, pos)) / fs::path(path);
+
+      if (fs::exists(fs::path(pPath)))
+      {
+        return fs::canonical(pPath)
+          .parent_path().parent_path().string();
+      }
+
+      fullPath.erase(0, pos + delimiter.length());
+    }
+  }
+
+  throw std::runtime_error(std::string("Could not find ") + path + std::string("."));
 }
 
 } // namespace util
