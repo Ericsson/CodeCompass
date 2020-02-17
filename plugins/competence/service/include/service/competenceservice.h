74db8445795aa0dfe26fee933510ae061000fa7b
Anett Fekete <anett.fekete@ericsson.com>
2020-02-17 16:29:45 +0100

    A lot is added to competence service.

--- plugins/competence/service/include/service/competenceservice.h
+++ plugins/competence/service/include/service/competenceservice.h
@@ -21,6 +21,7 @@ namespace competence
 
 typedef std::unique_ptr<git_blame, decltype(&git_blame_free)> BlamePtr;
 typedef std::unique_ptr<git_blame_options> BlameOptsPtr;
+typedef std::unique_ptr<git_commit, decltype(&git_commit_free)> CommitPtr;
 typedef std::unique_ptr<git_repository, decltype(&git_repository_free)> RepositoryPtr;
 
 class CompetenceServiceHandler : virtual public CompetenceServiceIf
@@ -35,11 +36,12 @@ public:
     const core::FileId& fileId_,
     const int ratio_) override;
 
-  void loadRepositoryData(
-    std::string& return_,
+  void loadRepositoryData(std::string& return_,
+    const core::FileId& fileId_,
     const std::string& repoId_,
     const std::string& hexOid_,
-    const std::string& path_);
+    const std::string& path_,
+    const std::string& user_ = "none");
 
 private:
   std::shared_ptr<odb::database> _db;
@@ -52,7 +54,10 @@ private:
     git_blame_options* opts_);
   BlameOptsPtr createBlameOpts(const git_oid& newCommitOid_);
   RepositoryPtr createRepository(const std::string& repoId_);
+  CommitPtr createCommit(git_repository *repo_,
+    const git_oid& id_);
   git_oid gitOidFromStr(const std::string& hexOid_);
+  std::string gitOidToString(const git_oid* oid_);
 
 };
 
--- plugins/competence/service/src/competenceservice.cpp
+++ plugins/competence/service/src/competenceservice.cpp
@@ -1,6 +1,7 @@
 #include <service/competenceservice.h>
 #include "../../model/include/model/filecomprehension.h"
 #include "../../../../model/include/model/file.h"
+#include "../include/service/competenceservice.h"
 
 #include <model/filecomprehension.h>
 #include <model/filecomprehension-odb.hxx>
@@ -36,9 +37,11 @@ void CompetenceServiceHandler::setCompetenceRatio(std::string& return_,
 }
 
 void CompetenceServiceHandler::loadRepositoryData(std::string& return_,
+  const core::FileId& fileId_,
   const std::string& repoId_,
   const std::string& hexOid_,
-  const std::string& path_)
+  const std::string& path_,
+  const std::string& user_)
 {
   _transaction([&, this](){
     RepositoryPtr repo = createRepository(repoId_);
@@ -49,6 +52,9 @@ void CompetenceServiceHandler::loadRepositoryData(std::string& return_,
     BlameOptsPtr opt = createBlameOpts(gitOidFromStr(hexOid_));
     BlamePtr blame = createBlame(repo.get(), path_.c_str(), opt.get());
 
+    std::uint32_t blameLines = 0;
+    std::uint32_t totalLines = 0;
+
     for (std::uint32_t i = 0; i < git_blame_get_hunk_count(blame.get()); ++i)
     {
       const git_blame_hunk* hunk = git_blame_get_hunk_byindex(blame.get(), i);
@@ -56,16 +62,56 @@ void CompetenceServiceHandler::loadRepositoryData(std::string& return_,
       GitBlameHunk blameHunk;
       blameHunk.linesInHunk = hunk->lines_in_hunk;
       blameHunk.boundary = hunk->boundary;
-      //blameHunk.finalCommitId = gitOidToString(&hunk->final_commit_id);
+      blameHunk.finalCommitId = gitOidToString(&hunk->final_commit_id);
       blameHunk.finalStartLineNumber = hunk->final_start_line_number;
 
       if (hunk->final_signature)
       {
         blameHunk.finalSignature.name = hunk->final_signature->name;
-        blameHunk.finalSignature.email = hunk->final_signature->email;
+        //blameHunk.finalSignature.email = hunk->final_signature->email;
         blameHunk.finalSignature.time = hunk->final_signature->when.time;
       }
+      // TODO
+      // git_oid_iszero is deprecated.
+      // It should be replaced with git_oid_is_zero in case of upgrading libgit2.
+      else if (!git_oid_iszero(&hunk->final_commit_id))
+      {
+        CommitPtr commit = createCommit(repo.get(), hunk->final_commit_id);
+        const git_signature* author = git_commit_author(commit.get());
+        blameHunk.finalSignature.name = author->name;
+        //blameHunk.finalSignature.email = author->email;
+        blameHunk.finalSignature.time = author->when.time;
+      }
+
+      if (blameHunk.finalSignature.time)
+      {
+        CommitPtr commit = createCommit(repo.get(), hunk->final_commit_id);
+      }
+
+      blameHunk.origCommitId = gitOidToString(&hunk->orig_commit_id);
+      blameHunk.origPath = hunk->orig_path;
+      blameHunk.origStartLineNumber = hunk->orig_start_line_number;
+      if (hunk->orig_signature)
+      {
+        blameHunk.origSignature.name = hunk->orig_signature->name;
+        blameHunk.origSignature.time = hunk->orig_signature->when.time;
+      }
+
+      if (!user_.compare(std::string(blameHunk.finalSignature.name)))
+      {
+        blameLines += blameHunk.linesInHunk;
+      }
+
+      totalLines += blameHunk.linesInHunk;
+
+      return_ = user_;
     }
+
+    model::FileComprehension fileComprehension;
+    fileComprehension.ratio = blameLines / totalLines;
+    fileComprehension.file = std::make_shared<model::File>();
+    fileComprehension.file->id = std::stoull(fileId_);
+    _db->persist(fileComprehension);
   });
 }
 
@@ -95,6 +141,19 @@ BlamePtr CompetenceServiceHandler::createBlame(
   return BlamePtr { blame, &git_blame_free };
 }
 
+CommitPtr CompetenceServiceHandler::createCommit(
+  git_repository* repo_,
+  const git_oid& id_)
+{
+  git_commit* commit = nullptr;
+  int error = git_commit_lookup(&commit, repo_, &id_);
+
+  if (error)
+    LOG(error) << "Getting commit failed: " << error;
+
+  return CommitPtr { commit, &git_commit_free };
+}
+
 BlameOptsPtr CompetenceServiceHandler::createBlameOpts(const git_oid& newCommitOid_)
 {
   git_blame_options* blameOpts = new git_blame_options;
@@ -114,6 +173,17 @@ git_oid CompetenceServiceHandler::gitOidFromStr(const std::string& hexOid_)
       << ") into a git_oid has been failed: " << error;
 
   return oid;
+}
+
+std::string CompetenceServiceHandler::gitOidToString(const git_oid* oid_)
+{
+  char oidstr[GIT_OID_HEXSZ + 1];
+  git_oid_tostr(oidstr, sizeof(oidstr), oid_);
+
+  if (!strlen(oidstr))
+    LOG(warning) << "Format a git_oid into a string has been failed.";
+
+  return std::string(oidstr);
 }
 
 } // competence
