a148eea6be252ea531341a930a676326852c5412
Anett Fekete <anett.fekete@ericsson.com>
2020-03-19 14:55:08 +0100

    Add basic competence diagram construction.

--- plugins/competence/service/src/diagram.cpp
+++ plugins/competence/service/src/diagram.cpp
@@ -7,19 +7,81 @@ namespace service
 namespace competence
 {
 
-Diagram::Diagram(
+CompetenceDiagram::CompetenceDiagram(
   std::shared_ptr<odb::database> db_,
   std::shared_ptr<std::string> datadir_,
   const cc::webserver::ServerContext& context_)
-    : _projectHandler(db_, datadir_, context_)
+    : _compHandler(db_, datadir_, context_),
+      _projectHandler(db_, datadir_, context_)
 {
 }
 
-void Diagram::getDirectoryCompetenceDiagram(
+void CompetenceDiagram::getDirectoryCompetenceDiagram(
   util::Graph& graph_,
   const core::AstNodeId& astNodeId_)
 {
 
+}
+
+void CompetenceDiagram::getFileCompetenceDiagram(
+  util::Graph& graph_,
+  const core::AstNodeId& astNodeId_)
+{
+  std::vector<language::AstNodeInfo> nodes;
+}
+
+util::Graph::Node CompetenceDiagram::addNode(
+  util::Graph& graph_,
+  const language::AstNodeInfo& nodeInfo_)
+{
+  util::Graph::Node node
+    = graph_.getOrCreateNode(nodeInfo_.id,
+      addSubgraph(graph_, nodeInfo_.range.file));
+
+  graph_.setNodeAttribute(node, "label", nodeInfo_.astNodeValue);
+
+  return node;
+}
+
+util::Graph::Subgraph CompetenceDiagram::addSubgraph(
+  util::Graph& graph_,
+  const core::FileId& fileId_)
+{
+  auto it = _subgraphs.find(fileId_);
+
+  if (it != _subgraphs.end())
+    return it->second;
+
+  core::FileInfo fileInfo;
+  _projectHandler.getFileInfo(fileInfo, fileId_);
+
+  util::Graph::Subgraph subgraph
+    = graph_.getOrCreateSubgraph("cluster_" + fileInfo.path);
+
+  graph_.setSubgraphAttribute(subgraph, "id", fileInfo.id);
+  graph_.setSubgraphAttribute(subgraph, "label", fileInfo.path);
+
+  _subgraphs.insert(it, std::make_pair(fileInfo.path, subgraph));
+
+  return subgraph;
+}
+
+void CompetenceDiagram::decorateNode(
+  util::Graph& graph_,
+  const util::Graph::Node& node_,
+  const Decoration& decoration_) const
+{
+  for (const auto& attr : decoration_)
+    graph_.setNodeAttribute(node_, attr.first, attr.second);
+}
+
+void CompetenceDiagram::decorateEdge(
+  util::Graph& graph_,
+  const util::Graph::Edge& edge_,
+  const Decoration& decoration_) const
+{
+  for (const auto& attr : decoration_)
+    graph_.setEdgeAttribute(edge_, attr.first, attr.second);
 }
 }
 }
--- plugins/competence/service/src/diagram.h
+++ plugins/competence/service/src/diagram.h
@@ -1,9 +1,11 @@
-#ifndef CODECOMPASS_DIAGRAM_H
-#define CODECOMPASS_DIAGRAM_H
+#ifndef CC_SERVICE_COMPETENCE_DIAGRAM_H
+#define CC_SERVICE_COMPETENCE_DIAGRAM_H
 
 #include <service/competenceservice.h>
 #include <projectservice/projectservice.h>
 #include <util/graph.h>
+
+#include "../../../../../../../cc/build/service/language/gen-cpp/language_types.h"
 
 namespace cc
 {
@@ -12,10 +14,10 @@ namespace service
 namespace competence
 {
 
-class Diagram
+class CompetenceDiagram
 {
 public:
-  Diagram(
+  CompetenceDiagram(
     std::shared_ptr<odb::database> db_,
     std::shared_ptr<std::string> datadir_,
     const cc::webserver::ServerContext& context_);
@@ -24,7 +26,34 @@ public:
     util::Graph& graph_,
     const core::AstNodeId& astNodeId_);
 
+  void getFileCompetenceDiagram(
+    util::Graph& graph_,
+    const core::AstNodeId& astNodeId_);
+
 private:
+  typedef std::vector<std::pair<std::string, std::string>> Decoration;
+
+  util::Graph::Node addNode(
+    util::Graph& graph_,
+    const language::AstNodeInfo& nodeInfo_);
+
+  util::Graph::Subgraph addSubgraph(
+    util::Graph& graph_,
+    const core::FileId& fileId_);
+
+  void decorateNode(
+    util::Graph& graph_,
+    const util::Graph::Node& node_,
+    const Decoration& decoration_) const;
+
+  void decorateEdge(
+    util::Graph& graph_,
+    const util::Graph::Edge& edge_,
+    const Decoration& decoration_) const;
+
+  std::map<core::FileId, util::Graph::Subgraph> _subgraphs;
+
+  CompetenceServiceHandler _compHandler;
   core::ProjectServiceHandler _projectHandler;
 
 };
@@ -36,4 +65,4 @@ private:
 
 
 
-#endif //CODECOMPASS_DIAGRAM_H
+#endif //CC_SERVICE_COMPETENCE_DIAGRAM_H
