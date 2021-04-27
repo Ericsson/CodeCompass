dfcc19e4cef59aa1e7d6e880b62e41be9589a5cd
Bence <rmfcnb@gmail.com>
2021-04-27 18:59:27 +0200

    Use config.json in main.py to parse projects.

--- plugins/python/parser/src/scripts/cc_python_parser/main.py
+++ plugins/python/parser/src/scripts/cc_python_parser/main.py
@@ -1,12 +1,38 @@
 import os
+import json
 from copy import copy
-from pathlib import PurePath
-from typing import Final
+from pathlib import PurePath, Path
+from typing import List, Final
 
 from cc_python_parser.common.metrics import metrics, PythonParserMetrics
 from cc_python_parser.parse_exception import ParseException
 from cc_python_parser.parser import Parser
 from cc_python_parser.persistence.persistence import ModelPersistence
+
+
+def read_config() -> List[str]:
+    """
+    Reads projects to parse from 'config.json', if it exists. Projects can be skipped with the "status" key:
+
+    {
+        "projects": [
+            {
+                "project": "project_path",
+                "status": true/false
+            }
+        ]
+    }
+    """
+
+    config_file_name: Final[str] = 'config.json'
+    project_roots = []
+    if Path(config_file_name).exists():
+        with open(config_file_name, 'r') as config_file:
+            config = json.load(config_file)
+            for project in config['projects']:
+                if project['status']:
+                    project_roots.append(project['project'])
+    return project_roots
 
 
 def main():
@@ -17,25 +43,7 @@ def main():
     def file_exception(path: PurePath) -> bool:
         return False
 
-    code_compass: Final[str] = "/home/rmfcnb/ELTE/Code-Compass-Python-Plugin"
-    code_checker: Final[str] = "/home/rmfcnb/ELTE/CodeChecker"
-    python_std: Final[str] = "/usr/lib/python3.8"
-    manim: Final[str] = "/home/rmfcnb/ELTE/manim"
-    numpy: Final[str] = "/home/rmfcnb/ELTE/numpy"
-    algo: Final[str] = "/home/rmfcnb/ELTE/TheAlgorithms"
-    tensorflow: Final[str] = "/home/rmfcnb/ELTE/tensorflow"
-
-    test: Final[str] = "/home/rmfcnb/ELTE/Test"
-
-    project_roots = [code_compass,
-                     code_checker,
-                     python_std,
-                     manim,
-                     numpy,
-                     algo,
-                     tensorflow]
-
-    project_roots = [test]
+    project_roots = read_config()
 
     metrics_result = []
     for pr in project_roots:
@@ -52,8 +60,6 @@ def main():
         all_metrics += m
 
     print(all_metrics)
-
-    pass
 
 
 if __name__ == '__main__':
