713cd364a3a70f4690f45c9f3a33e8f6175b582f
Wrathan <hth.gergo@gmail.com>
2021-05-26 13:13:44 +0200

    Trying to add xml to javamodel.jar. (Does not work yet)

--- plugins/java/model/JavaTypedef.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "\"JavaTypedef\"")
-public class JavaTypedef extends JavaTypedEntity {
-}
