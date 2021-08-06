31d38938539eebfd58d3a3f30d718b08a1830791
Anett Fekete <anett.fekete@ericsson.com>
2021-08-06 21:09:54 +0200

    Update build-codecompass.sh

--- .gitlab/build-codecompass.sh
+++ .gitlab/build-codecompass.sh
@@ -63,7 +63,7 @@ cmake $CC_SRC_DIR \
   -DCMAKE_INSTALL_PREFIX=$CC_INSTALL_DIR \
   -DDATABASE=pgsql \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DWITH_AUTH="plain;ldap" \
   -DLLVM_DIR=$DEPS_INSTALL_RUNTIME_DIR/llvm/lib/cmake/llvm/ \
-  -DClang_DIR=$DEPS_INSTALL_RUNTIME_DIR/llvm/lib/cmake/clang \
-  -DWITH_AUTH="plain;ldap"
+  -DClang_DIR=$DEPS_INSTALL_RUNTIME_DIR/llvm/lib/cmake/clang/
 make install --quiet --jobs $(nproc)
