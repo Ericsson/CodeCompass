aa0bd2596deb3635d4dedb2e58bb0e2255753439
Rizwan Hussain <rizwankadhar@gmail.com>
2022-03-26 16:22:37 +0100

    Added Model layer of Yaml Parser
    ^

---
+++ plugins/yaml/model/include/model/yaml.h
@@ -0,0 +1,40 @@
+#ifndef CC_MODEL_YAML_H
+#define CC_MODEL_YAML_H
+
+#include <string>
+
+#include <odb/core.hxx>
+#include <odb/lazy-ptr.hxx>
+#include <odb/nullable.hxx>
+
+#include <model/file.h>
+
+namespace cc
+{
+namespace model
+{
+
+#pragma db object
+struct YAML
+{
+  enum Type
+  {
+    HELM_CHART,
+    OTHER
+  };
+
+
+  #pragma db id auto
+  std::uint64_t id;
+
+  #pragma db not_null
+  FileId file;
+
+  #pragma db not_null
+  Type type;
+};
+
+} //model
+} //cc
+
+#endif // CC_MODEL_METRICS_H
