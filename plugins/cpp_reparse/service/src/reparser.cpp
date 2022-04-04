10de27657453eb1d484ec6ce2387340ae2252a5d
András Dóczi <andocz@hotmail.com>
2022-04-04 20:52:41 +0200

    Use saved build directory when invoking Clang tool in reparser

--- plugins/cpp_reparse/service/src/reparser.cpp
+++ plugins/cpp_reparse/service/src/reparser.cpp
@@ -4,6 +4,8 @@
 #include <clang/Tooling/CompilationDatabase.h>
 #include <clang/Tooling/Tooling.h>
 
+#include <model/builddirectory.h>
+#include <model/builddirectory-odb.hxx>
 #include <model/buildsourcetarget.h>
 #include <model/buildsourcetarget-odb.hxx>
 #include <model/file.h>
@@ -19,6 +21,7 @@
 namespace
 {
 
+typedef odb::query<cc::model::BuildDirectory> BuildDirQuery;
 typedef odb::query<cc::model::BuildSource> BuildSourceQuery;
 typedef odb::result<cc::model::BuildSource> BuildSourceResult;
 typedef odb::query<cc::model::File> FileQuery;
@@ -64,14 +67,14 @@ boost::variant<
 CppReparser::getCompilationCommandForFile(
   const core::FileId& fileId_)
 {
-  std::string buildCommand;
+  cc::model::BuildActionPtr buildAction;
 
   _transaction([&, this](){
     BuildSourceResult bss = _db->query<model::BuildSource>(
       BuildSourceQuery::file == std::stoull(fileId_));
     for (auto bs : bss)
-      if (buildCommand.empty())
-        buildCommand = bs.action->command;
+      if (!buildAction || buildAction->command.empty())
+        buildAction = bs.action;
       else
       {
         LOG(warning) << "Multiple build commands present for source file #"
@@ -83,17 +86,32 @@ CppReparser::getCompilationCommandForFile(
       }
   });
 
-  if (buildCommand.empty())
+  if (!buildAction)
   {
     return "Build command not found for the file! Is this not a C++ file, "
       "or a header?";
   }
 
+  std::string buildDir;
+
+  _transaction([&, this](){
+    cc::model::BuildDirectoryPtr bd = _db->query_one<model::BuildDirectory>(
+      BuildDirQuery::action == buildAction->id);
+    if (bd)
+      buildDir = bd->directory;
+    else
+    {
+      LOG(debug) << "No build directory for source file #"
+                 << std::stoull(fileId_) << ". Using '/'.";
+      buildDir = "/";
+    }
+  });
+
   //--- Assemble compiler command line ---//
 
   std::vector<const char*> commandLine;
   std::vector<std::string> split;
-  boost::split(split, buildCommand, boost::is_any_of(" "));
+  boost::split(split, buildAction->command, boost::is_any_of(" "));
 
   commandLine.reserve(split.size());
   commandLine.push_back("--");
@@ -133,7 +151,8 @@ CppReparser::getCompilationCommandForFile(
     FixedCompilationDatabase::loadFromCommandLine(
       argc,
       commandLine.data(),
-      compilationDbLoadError));
+      compilationDbLoadError,
+      buildDir));
 
   if (!compilationDb)
   {
@@ -164,7 +183,8 @@ CppReparser::getASTForTranslationUnitFile(
     IntrusiveRefCntPtr<DatabaseFileSystem> dbfs(
       new DatabaseFileSystem(_db));
     IntrusiveRefCntPtr<llvm::vfs::OverlayFileSystem> overlayFs(
-      new llvm::vfs::OverlayFileSystem(llvm::vfs::getRealFileSystem()));
+      new llvm::vfs::OverlayFileSystem(
+        llvm::vfs::createPhysicalFileSystem().release()));
     overlayFs->pushOverlay(dbfs);
 
     auto compileDb = std::move(
