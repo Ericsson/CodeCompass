4de5e096e52454377e0505b71727b64ee286f600
mdeme01 <se4n01@gmail.com>
2023-01-12 15:38:57 +0100

    feat(layout): codemirror skeleton

--- webgui-new/src/components/pages/project/project.tsx
+++ webgui-new/src/components/pages/project/project.tsx
@@ -1,11 +1,69 @@
+import ReactCodeMirror from '@uiw/react-codemirror';
+import { okaidia } from '@uiw/codemirror-theme-okaidia';
 import { Header } from '../../lib/header/header';
 import { SidebarMenu } from '../../lib/sidebar-menu/sidebar-menu';
+import { styled } from '@mui/material';
+import { cpp } from '@codemirror/lang-cpp';
+
+const MainContainer = styled('div')({
+  display: 'flex',
+});
+
+const placeholder = `
+#include <stdio.h>
+#include <stdlib.h>
+
+char* readStr() {
+  char* str;
+
+  size_t len = 0;
+  size_t size = 10;
+  char ch;
+
+  str = realloc(NULL, sizeof(*str) * size);
+
+  while (((ch = fgetc(stdin)) != EOF) && (ch != '\n')) {
+    str[len++] = ch;
+
+    if (len == size) {
+      size += 16;
+      str = realloc(str, sizeof(*str) * size);
+    }
+  }
+
+  str[len++] = '\0';
+  str = realloc(str, sizeof(*str) * len);
+
+  return str;
+}
+
+int main() {
+  printf("Input: ");
+  char* str = readStr();
+  
+  printf("Output: %s\n", str);
+  free(str);
+
+  return 0;
+}
+`;
 
 export const Project = (): JSX.Element => {
   return (
     <>
       <Header />
-      <SidebarMenu />
+      <MainContainer>
+        <SidebarMenu />
+        <ReactCodeMirror
+          readOnly={true}
+          extensions={[cpp()]}
+          theme={okaidia}
+          style={{ flexGrow: '1' }}
+          value={placeholder}
+          width={'100%'}
+          height={'100%'}
+        />
+      </MainContainer>
     </>
   );
 };
