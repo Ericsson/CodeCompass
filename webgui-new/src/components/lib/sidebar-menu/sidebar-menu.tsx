1abd6a87d7735d2dc4d308f92ba0417f168d39e7
mdeme01 <se4n01@gmail.com>
2023-01-13 10:56:26 +0100

    feat(sidebar): collapse, icons

--- webgui-new/src/components/lib/sidebar-menu/sidebar-menu.tsx
+++ webgui-new/src/components/lib/sidebar-menu/sidebar-menu.tsx
@@ -1,14 +1,36 @@
-import { styled } from '@mui/material';
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { IconButton, styled } from '@mui/material';
+import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar';
+import { ArrowCircleRight, ArrowCircleLeft, Folder, Search, Info, GitHub } from '@mui/icons-material';
 
-const SidebarContainer = styled('div')({
-  height: '90vh',
+const SidebarContainer = styled('div')({});
+
+const SidebarHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '1rem',
+  borderBottom: `1px solid ${theme.colors?.primary}`,
+  borderRight: `1px solid ${theme.colors?.primary}`,
+}));
+
+const IconLabel = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.6rem',
 });
 
 export const SidebarMenu = (): JSX.Element => {
+  const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } = useProSidebar();
+
   return (
     <SidebarContainer>
-      <Sidebar style={{ height: '100%' }}>
+      <SidebarHeader>
+        {collapsed ? '' : 'Menu'}
+        <IconButton onClick={() => collapseSidebar()}>
+          {collapsed ? <ArrowCircleRight /> : <ArrowCircleLeft />}
+        </IconButton>
+      </SidebarHeader>
+      <Sidebar>
         <Menu
           menuItemStyles={{
             button: ({ level, active, disabled }) => {
@@ -23,18 +45,46 @@ export const SidebarMenu = (): JSX.Element => {
             },
           }}
         >
-          <SubMenu label='File manager'>
+          <SubMenu
+            label={
+              <IconLabel>
+                <Folder />
+                {collapsed ? '' : 'File manager'}
+              </IconLabel>
+            }
+          >
             <MenuItem>auth.c</MenuItem>
             <MenuItem>auth.h</MenuItem>
           </SubMenu>
-          <SubMenu label='Search results'>
+          <SubMenu
+            label={
+              <IconLabel>
+                <Search />
+                {collapsed ? '' : 'Search results'}
+              </IconLabel>
+            }
+          >
             <MenuItem>main.c</MenuItem>
           </SubMenu>
-          <SubMenu label='Info tree'>
+          <SubMenu
+            label={
+              <IconLabel>
+                <Info />
+                {collapsed ? '' : 'Info tree'}
+              </IconLabel>
+            }
+          >
             <MenuItem>Info 1</MenuItem>
             <MenuItem>Info 2</MenuItem>
           </SubMenu>
-          <SubMenu label='Revision control navigator'>
+          <SubMenu
+            label={
+              <IconLabel>
+                <GitHub />
+                {collapsed ? '' : 'Revision control'}
+              </IconLabel>
+            }
+          >
             <MenuItem>Content</MenuItem>
           </SubMenu>
         </Menu>
