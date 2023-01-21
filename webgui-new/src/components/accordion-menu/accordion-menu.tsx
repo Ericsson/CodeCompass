6de47573bc022df2bfaa55aec109d49351b320ea
mdeme01 <se4n01@gmail.com>
2023-01-21 12:36:36 +0100

    refactor(accordion): styling

--- webgui-new/src/components/accordion-menu/accordion-menu.tsx
+++ webgui-new/src/components/accordion-menu/accordion-menu.tsx
@@ -1,30 +1,44 @@
 import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import { ArrowForwardIosSharp, Folder, Search, Info, GitHub } from '@mui/icons-material';
 import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
 import MuiAccordionSummary, { AccordionSummaryProps } from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 
-const Container = styled('div')(({ theme }) => ({
-  borderRight: `1px solid ${theme.colors?.primary}`,
+const Container = styled('div')({
   gridArea: 'sidebar',
-}));
+});
+
+const IconLabel = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.5rem',
+});
 
 const Accordion = styled((props: AccordionProps) => <MuiAccordion disableGutters elevation={0} square {...props} />)(
   ({ theme }) => ({
-    // border: `1px solid ${theme.colors?.primary}`,
+    border: `1px solid ${theme.colors?.primary}`,
+    '&:nth-of-type(1)': {
+      borderTop: '0',
+    },
     '&:not(:last-child)': {
       borderBottom: 0,
     },
     '&:before': {
       display: 'none',
     },
+    '&:nth-of-type(1) > div': {
+      borderTop: '0',
+    },
+    '&:last-child > div': {
+      borderBottom: '0',
+    },
   })
 );
 
 const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />} {...props} />
+  <MuiAccordionSummary expandIcon={<ArrowForwardIosSharp sx={{ fontSize: '0.9rem' }} />} {...props} />
 ))(({ theme }) => ({
   color: theme.colors?.primary,
   backgroundColor: theme.backgroundColors?.primary,
@@ -40,68 +54,59 @@ const AccordionSummary = styled((props: AccordionSummaryProps) => (
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   color: theme.colors?.primary,
   backgroundColor: theme.backgroundColors?.primary,
+  borderTop: `1px solid ${theme.colors?.primary}`,
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
   height: '70vh',
 }));
 
 export const AccordionMenu = () => {
-  const [expanded, setExpanded] = useState<string | false>('panel1');
+  const [expanded, setExpanded] = useState<string>('panel1');
 
-  const handleChange = (panel: string) => (_e: React.SyntheticEvent, newExpanded: boolean) => {
-    // setExpanded(newExpanded ? panel : false);
-    setExpanded(panel);
-  };
+  const placeholder = (
+    <Typography>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo
+      lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
+      blandit leo lobortis eget.
+    </Typography>
+  );
 
   return (
     <Container>
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
-          <Typography>{'File manager'}</Typography>
+      <Accordion expanded={expanded === 'panel1'} onChange={() => setExpanded('panel1')}>
+        <AccordionSummary aria-controls={'panel1-controls'} id={'panel1-header'}>
+          <IconLabel>
+            <Folder />
+            <Typography>{'File manager'}</Typography>
+          </IconLabel>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
-            leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
+        <AccordionDetails>{placeholder}</AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <AccordionSummary aria-controls='panel2d-content' id='panel2d-header'>
-          <Typography>{'Search results'}</Typography>
+      <Accordion expanded={expanded === 'panel2'} onChange={() => setExpanded('panel2')}>
+        <AccordionSummary aria-controls={'panel2-controls'} id={'panel2-header'}>
+          <IconLabel>
+            <Search />
+            <Typography>{'Search results'}</Typography>
+          </IconLabel>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
-            leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
+        <AccordionDetails>{placeholder}</AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <AccordionSummary aria-controls='panel3d-content' id='panel3d-header'>
-          <Typography>{'Info tree'}</Typography>
+      <Accordion expanded={expanded === 'panel3'} onChange={() => setExpanded('panel3')}>
+        <AccordionSummary aria-controls={'panel3-controls'} id={'panel3-header'}>
+          <IconLabel>
+            <Info />
+            <Typography>{'Info tree'}</Typography>
+          </IconLabel>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
-            leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
+        <AccordionDetails>{placeholder}</AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-        <AccordionSummary aria-controls='panel4d-content' id='panel4d-header'>
-          <Typography>{'Revision control navigator'}</Typography>
+      <Accordion expanded={expanded === 'panel4'} onChange={() => setExpanded('panel4')}>
+        <AccordionSummary aria-controls={'panel4-controls'} id={'panel4-header'}>
+          <IconLabel>
+            <GitHub />
+            <Typography>{'Revision control navigator'}</Typography>
+          </IconLabel>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
-            leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
+        <AccordionDetails>{placeholder}</AccordionDetails>
       </Accordion>
     </Container>
   );
