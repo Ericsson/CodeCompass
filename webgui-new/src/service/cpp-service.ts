cc4b5a200902689a21751b973a5b58d100a8cfb6
mdeme01 <se4n01@gmail.com>
2023-03-24 11:53:16 +0100

    feat(*): cpp service methods

--- webgui-new/src/service/cpp-service.ts
+++ webgui-new/src/service/cpp-service.ts
@@ -1,5 +1,5 @@
 import thrift from 'thrift';
-import { LanguageService } from '@thrift-generated';
+import { LanguageService, FilePosition, Position } from '@thrift-generated';
 import { config } from './config';
 
 let client: LanguageService.Client | undefined;
@@ -13,6 +13,13 @@ export const createCppClient = (workspace: string) => {
   });
   client = thrift.createXHRClient(LanguageService, connection);
   return client;
+};
+
+export const getCppFileTypes = async () => {
+  if (!client) {
+    return [];
+  }
+  return await client.getFileTypes();
 };
 
 export const getCppFileDiagramTypes = async (fileId: string) => {
@@ -47,5 +54,207 @@ export const getCppFileDiagramLegend = async (diagramId: number) => {
     return await client.getFileDiagramLegend(diagramId);
   } catch {
     return '';
+  }
+};
+
+export const getCppDiagramTypes = async (astNodeId: string) => {
+  let resultMap = new Map<string, number>();
+  if (!client) {
+    return resultMap;
+  }
+  try {
+    resultMap = await client.getDiagramTypes(astNodeId);
+  } catch {
+    resultMap = new Map();
+  }
+  return resultMap;
+};
+
+export const getCppDiagram = async (astNodeId: string, diagramId: number) => {
+  if (!client) {
+    return '';
+  }
+  try {
+    return await client.getDiagram(astNodeId, diagramId);
+  } catch {
+    return '';
+  }
+};
+
+export const getCppDiagramLegend = async (diagramId: number) => {
+  if (!client) {
+    return '';
+  }
+  try {
+    return await client.getDiagramLegend(diagramId);
+  } catch {
+    return '';
+  }
+};
+
+export const getCppFileReferenceTypes = async (fileId: string) => {
+  let resultMap = new Map<string, number>();
+  if (!client) {
+    return resultMap;
+  }
+  try {
+    resultMap = await client.getFileReferenceTypes(fileId);
+  } catch {
+    resultMap = new Map();
+  }
+  return resultMap;
+};
+
+export const getCppFileReferences = async (fileId: string, referenceId: number) => {
+  if (!client) {
+    return [];
+  }
+  try {
+    return await client.getFileReferences(fileId, referenceId);
+  } catch {
+    return [];
+  }
+};
+
+export const getCppFileReferenceCount = async (fileId: string, referenceId: number) => {
+  if (!client) {
+    return 0;
+  }
+  try {
+    return await client.getFileReferenceCount(fileId, referenceId);
+  } catch {
+    return 0;
+  }
+};
+
+export const getCppReferenceTypes = async (astNodeId: string) => {
+  let resultMap = new Map<string, number>();
+  if (!client) {
+    return resultMap;
+  }
+  try {
+    resultMap = await client.getReferenceTypes(astNodeId);
+  } catch {
+    resultMap = new Map();
+  }
+  return resultMap;
+};
+
+export const getCppReferences = async (astNodeId: string, referenceId: number, tags: string[]) => {
+  if (!client) {
+    return [];
+  }
+  try {
+    return await client.getReferences(astNodeId, referenceId, tags);
+  } catch {
+    return [];
+  }
+};
+
+export const getCppReferenceCount = async (astNodeId: string, referenceId: number) => {
+  if (!client) {
+    return 0;
+  }
+  try {
+    return await client.getReferenceCount(astNodeId, referenceId);
+  } catch {
+    return 0;
+  }
+};
+
+export const getCppReferencesInFile = async (
+  astNodeId: string,
+  referenceId: number,
+  fileId: string,
+  tags: string[]
+) => {
+  if (!client) {
+    return [];
+  }
+  try {
+    return await client.getReferencesInFile(astNodeId, referenceId, fileId, tags);
+  } catch {
+    return [];
+  }
+};
+
+export const getCppReferencesPage = async (
+  astNodeId: string,
+  referenceId: number,
+  pageSize: number,
+  pageNo: number
+) => {
+  if (!client) {
+    return [];
+  }
+  try {
+    return await client.getReferencesPage(astNodeId, referenceId, pageSize, pageNo);
+  } catch {
+    return [];
+  }
+};
+
+export const getCppSourceText = async (astNodeId: string) => {
+  if (!client) {
+    return '';
+  }
+  try {
+    return await client.getSourceText(astNodeId);
+  } catch {
+    return '';
+  }
+};
+
+export const getCppProperties = async (astNodeId: string) => {
+  let resultMap = new Map<string, string>();
+  if (!client) {
+    return resultMap;
+  }
+  try {
+    resultMap = await client.getProperties(astNodeId);
+  } catch {
+    resultMap = new Map();
+  }
+  return resultMap;
+};
+
+export const getCppDocumentation = async (astNodeId: string) => {
+  if (!client) {
+    return '';
+  }
+  try {
+    return await client.getDocumentation(astNodeId);
+  } catch {
+    return '';
+  }
+};
+
+export const getCppAstNodeInfo = async (fileId: string) => {
+  if (!client) {
+    return;
+  }
+  try {
+    return await client.getAstNodeInfo(fileId);
+  } catch {
+    return;
+  }
+};
+
+export const getCppAstNodeInfoByPosition = async (fileId: string, line: number, column: number) => {
+  if (!client) {
+    return;
+  }
+  try {
+    return await client.getAstNodeInfoByPosition(
+      new FilePosition({
+        file: fileId,
+        pos: new Position({
+          line,
+          column,
+        }),
+      })
+    );
+  } catch {
+    return;
   }
 };
