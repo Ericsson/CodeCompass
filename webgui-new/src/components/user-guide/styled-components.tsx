a0cc7e0951633eaeff8c91c7888ac326317b1db9
mdeme01 <se4n01@gmail.com>
2023-05-12 13:10:14 +0200

    feat(*): add user guide

--- webgui-new/src/components/user-guide/styled-components.tsx
+++ webgui-new/src/components/user-guide/styled-components.tsx
@@ -2,4 +2,22 @@ import { styled } from '@mui/material';
 
 export const OuterContainer = styled('div')({
   padding: '10px',
+  width: 'calc(100vw - 280px)',
+  height: 'calc(100vh - 78px - 48px)',
+  overflow: 'scroll',
+});
+
+export const StyledParagraph = styled('p')({
+  width: '60%',
+  marginBottom: '5xp',
+});
+
+export const StyledHeading1 = styled('h1')({
+  fontSize: '1.5rem',
+  marginBottom: '20px',
+});
+
+export const StyledHeading2 = styled('h2')({
+  fontSize: '1.2rem',
+  margin: '10px 0',
 });
