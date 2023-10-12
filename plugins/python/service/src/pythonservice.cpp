d2a788aded3f78972416fafaf36fcdb5767c857d
Barnabás Domozi <wbqpk3@inf.elte.hu>
2023-10-12 23:53:43 +0200

    PythonService basic implementations

--- plugins/python/service/src/pythonservice.cpp
+++ plugins/python/service/src/pythonservice.cpp
@@ -12,10 +12,165 @@ PythonServiceHandler::PythonServiceHandler(
   std::shared_ptr<odb::database> db_,
   std::shared_ptr<std::string> /*datadir_*/,
   const cc::webserver::ServerContext& context_)
-    : _db(db_), _transaction(db_), _context(context_)
+    : _db(db_), _transaction(db_), _context(context_) {}
+
+void PythonServiceHandler::getFileTypes(
+  std::vector<std::string>& return_) 
 {
+  return;
 }
 
-} // python
+void PythonServiceHandler::getAstNodeInfo(
+  AstNodeInfo& return_,
+  const core::AstNodeId& astNodeId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getAstNodeInfoByPosition(
+  AstNodeInfo& return_,
+  const core::FilePosition& fpos_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getSourceText(
+  std::string& return_,
+  const core::AstNodeId& astNodeId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getDocumentation(
+  std::string& return_,
+  const core::AstNodeId& astNodeId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getProperties(
+  std::map<std::string, std::string>& return_,
+  const core::AstNodeId& astNodeId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getDiagramTypes(
+  std::map<std::string, std::int32_t>& return_,
+  const core::AstNodeId& astNodeId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getDiagram(
+  std::string& return_,
+  const core::AstNodeId& astNodeId_,
+  const std::int32_t diagramId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getDiagramLegend(
+  std::string& return_,
+  const std::int32_t diagramId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getFileDiagramTypes(
+  std::map<std::string, std::int32_t>& return_,
+  const core::FileId& fileId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getFileDiagram(
+  std::string& return_,
+  const core::FileId& fileId_,
+  const int32_t diagramId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getFileDiagramLegend(
+  std::string& return_,
+  const std::int32_t diagramId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getReferenceTypes(
+  std::map<std::string, std::int32_t>& return_,
+  const core::AstNodeId& astNodeId) 
+{
+  return;
+}
+
+void PythonServiceHandler::getReferences(
+  std::vector<AstNodeInfo>& return_,
+  const core::AstNodeId& astNodeId_,
+  const std::int32_t referenceId_,
+  const std::vector<std::string>& tags_) 
+{
+  return;
+}
+
+std::int32_t PythonServiceHandler::getReferenceCount(
+  const core::AstNodeId& astNodeId_,
+  const std::int32_t referenceId_) 
+{
+  return 0;
+}
+
+void PythonServiceHandler::getReferencesInFile(
+  std::vector<AstNodeInfo>& return_,
+  const core::AstNodeId& astNodeId_,
+  const std::int32_t referenceId_,
+  const core::FileId& fileId_,
+  const std::vector<std::string>& tags_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getReferencesPage(
+  std::vector<AstNodeInfo>& return_,
+  const core::AstNodeId& astNodeId_,
+  const std::int32_t referenceId_,
+  const std::int32_t pageSize_,
+  const std::int32_t pageNo_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getFileReferenceTypes(
+  std::map<std::string, std::int32_t>& return_,
+  const core::FileId& fileId_) 
+{
+  return;
+}
+
+void PythonServiceHandler::getFileReferences(
+  std::vector<AstNodeInfo>& return_,
+  const core::FileId& fileId_,
+  const std::int32_t referenceId_) 
+{
+  return;
+}
+
+std::int32_t PythonServiceHandler::getFileReferenceCount(
+  const core::FileId& fileId_,
+  const std::int32_t referenceId_) 
+{
+  return 0;
+}
+
+void PythonServiceHandler::getSyntaxHighlight(
+  std::vector<SyntaxHighlight>& return_,
+  const core::FileRange& range_) 
+{
+  return;
+}
+
+} // language
 } // service
 } // cc
