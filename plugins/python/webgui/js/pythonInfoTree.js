0befbc486e3c44b1e6f41da18fc61ce348d4e0c6
Barnabás Domozi <wbqpk3@inf.elte.hu>
2023-10-16 20:05:31 +0200

    Python plugin webgui infotree

--- plugins/python/webgui/js/pythonInfoTree.js
+++ plugins/python/webgui/js/pythonInfoTree.js
@@ -3,6 +3,6 @@ require([
     'codecompass/viewHandler',
     'codecompass/util'],
 function (model, viewHandler, util) {  
-    model.addService('cppservice', 'CppService', LanguageServiceClient);
+    model.addService('pythonservice', 'PythonService', LanguageServiceClient);
 });
   
