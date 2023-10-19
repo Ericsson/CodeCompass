794ada953d8f638300c5012c9a61fd73cd6d4e86
Barnabás Domozi <wbqpk3@inf.elte.hu>
2023-10-19 17:46:38 +0200

    Added Python InfoTree and Menu

--- plugins/python/webgui/js/pythonMenu.js
+++ plugins/python/webgui/js/pythonMenu.js
@@ -8,5 +8,99 @@ require([
   'codecompass/urlHandler',
   'codecompass/viewHandler'],
 function (topic, Menu, MenuItem, PopupMenuItem, astHelper, model, urlHandler, viewHandler) {
+
   model.addService('pythonservice', 'PythonService', LanguageServiceClient);
+
+  var getdefintion = {
+    id : 'python-text-getdefintion',
+    render : function (nodeInfo, fileInfo) {
+      return new MenuItem({
+        label : 'Jump to definition',
+        accelKey : 'ctrl - click',
+        onClick : function () {
+          if (!nodeInfo || !fileInfo)
+            return;
+
+          var languageService = model.getLanguageService(fileInfo.type);
+          astHelper.jumpToDef(nodeInfo.id, model.pythonservice);
+
+          if (window.gtag) {
+            window.gtag ('event', 'jump_to_def', {
+              'event_category' : urlHandler.getState('wsid'),
+              'event_label' : urlHandler.getFileInfo().name
+                  + ': '
+                  + nodeInfo.astNodeValue
+            });
+          }
+        }
+      });
+    }
+  };
+
+  viewHandler.registerModule(getdefintion, {
+    type : viewHandler.moduleType.TextContextMenu,
+    service : model.pythonservice
+  });
+
+  var infoTree = {
+    id : 'python-text-infotree',
+    render : function (nodeInfo, fileInfo) {
+      return new MenuItem({
+        label : 'Info Tree',
+        onClick : function () {
+          if (!nodeInfo || !fileInfo)
+            return;
+
+          topic.publish('codecompass/infotree', {
+            fileType : fileInfo.type,
+            elementInfo : nodeInfo
+          });
+
+          if (window.gtag) {
+              window.gtag ('event', 'info_tree', {
+              'event_category' : urlHandler.getState('wsid'),
+              'event_label' : urlHandler.getFileInfo().name
+                  + ': '
+                  + nodeInfo.astNodeValue
+            });
+          }
+        }
+      });
+    }
+  };
+
+  viewHandler.registerModule(infoTree, {
+    type : viewHandler.moduleType.TextContextMenu,
+    service : model.pythonservice
+  });
+
+  var infobox = {
+    id : 'python-text-infobox',
+    render : function (nodeInfo, fileInfo) {
+      return new MenuItem({
+        label : 'Documentation',
+        onClick : function () {
+          topic.publish('codecompass/documentation', {
+            fileType    : fileInfo.type,
+            elementInfo : nodeInfo
+          });
+
+          if (window.gtag) {
+              window.gtag ('event', 'documentation', {
+              'event_category' : urlHandler.getState('wsid'),
+              'event_label' : urlHandler.getFileInfo().name
+                            + ': '
+                            + nodeInfo.astNodeValue
+            });
+          }
+        }
+      });
+    }
+  };
+
+  viewHandler.registerModule(infobox, {
+    type : viewHandler.moduleType.TextContextMenu,
+    service : model.pythonservice
+  });
+
 });
