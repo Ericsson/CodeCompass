920c810c308c6924e78b314b2077f196a30ed745
Mate Szabolcs Kiss <hello@kissmateszabolcs.hu>
2023-11-08 04:47:40 +0000

    ci: remove thrift from ubuntu 20.04 APT dependencies

--- .github/scripts/ubuntu-20.04/setup_build.sh
+++ .github/scripts/ubuntu-20.04/setup_build.sh
@@ -2,5 +2,5 @@
 
 # Install required packages for CodeCompass build
 sudo apt-get install -y git cmake make g++ libboost-all-dev llvm-11-dev clang-11 \
-  libclang-11-dev odb libodb-dev thrift-compiler libthrift-dev default-jdk libssl-dev \
+  libclang-11-dev odb libodb-dev default-jdk libssl-dev \
   libgraphviz-dev libmagic-dev libgit2-dev ctags doxygen libgtest-dev npm libldap2-dev
