68679caeb5b792ec0a09665e5934f62418f2938f
Mate Szabolcs Kiss <hello@kissmateszabolcs.hu>
2023-11-12 06:07:15 +0000

    fix: remove unnecessary step

--- .github/scripts/ubuntu-20.04/postcompile_runtime.sh
+++
@@ -1,3 +0,0 @@
-# Post compilation configuration for runtime (environmental variables, library location settings etc..)
-
-echo "CMAKE_PREFIX_PATH=${INSTALL_PATH}/thrift/lib:$CMAKE_PREFIX_PATH" >> $GITHUB_ENV
