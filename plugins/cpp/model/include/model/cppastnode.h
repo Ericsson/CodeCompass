9ee55af7685895a0656c69a54a67548c7bebb12d
Bükki Dániel <dbukki@freemail.hu>
2023-11-26 15:43:36 +0100

    Further refactor of queries via db views.

--- plugins/cpp/model/include/model/cppastnode.h
+++ plugins/cpp/model/include/model/cppastnode.h
@@ -237,6 +237,30 @@ struct CppAstCount
   std::size_t count;
 };
 
+#pragma db view \
+  object(CppAstNode) \
+  object(File : CppAstNode::location.file) \
+  query((CppAstNode::astType == cc::model::CppAstNode::AstType::Read \
+      || CppAstNode::astType == cc::model::CppAstNode::AstType::Write) && (?))
+struct CppRWAstNodeWithHashAndLoc
+{
+  typedef cc::model::Position::PosType PosType;
+
+  #pragma db column(CppAstNode::entityHash)
+  std::uint64_t entityHash;
+
+  #pragma db column(CppAstNode::location.range.start.line)
+  PosType startLine;
+  #pragma db column(CppAstNode::location.range.start.column)
+  PosType startColumn;
+  #pragma db column(CppAstNode::location.range.end.line)
+  PosType endLine;
+  #pragma db column(CppAstNode::location.range.end.column)
+  PosType endColumn;
+
+  #pragma db column(File::path)
+  std::string filePath;
+};
 }
 }
 
