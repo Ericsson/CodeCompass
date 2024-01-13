36cd9c2e4fd4b876f421e50cc6f6372ca19edad3
Máté Cserép <mcserep@gmail.com>
2024-01-13 13:58:29 +0100

    Upgrade LLVM to v15.

--- .github/scripts/ubuntu-22.04/setup_build.sh
+++ .github/scripts/ubuntu-22.04/setup_build.sh
@@ -2,7 +2,7 @@
 
 # Install required packages for CodeCompass build
 sudo apt install git cmake make g++ libboost-all-dev \
-  llvm-11-dev clang-11 libclang-11-dev \
+  llvm-15-dev clang-15 libclang-15-dev \
   gcc-11-plugin-dev thrift-compiler libthrift-dev \
   default-jdk libssl-dev libgraphviz-dev libmagic-dev libgit2-dev exuberant-ctags doxygen \
   libldap2-dev libgtest-dev
