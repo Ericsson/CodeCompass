97423ba7461b9f1db1d0290b2b624fa0f1116801
Máté Cserép <mcserep@gmail.com>
2024-02-03 15:35:56 +0100

    Clean up dependency sources and intermediate binaries in GitHub Actions CI to save space

--- .github/scripts/ubuntu-22.04/postcompile_build.sh
+++ .github/scripts/ubuntu-22.04/postcompile_build.sh
@@ -4,3 +4,6 @@
 
 echo "${INSTALL_PATH}/odb/bin" >> $GITHUB_PATH
 echo "CMAKE_PREFIX_PATH=${INSTALL_PATH}/odb:$CMAKE_PREFIX_PATH" >> $GITHUB_ENV
+
+# Clean up dependency sources and intermediate binaries to save space
+rm -rf ${DOWNLOAD_PATH}/odb
