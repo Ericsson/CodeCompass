9cbe592ee911bd97ac8ab3a4e480b6b64bada867
Máté Cserép <mcserep@gmail.com>
2024-02-03 17:29:42 +0100

    Maximize disk space in parse GitHub Action job. Remove unused steps from CI.

--- .github/scripts/ubuntu-20.04/postcompile_runtime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Post compilation configuration for runtime (environmental variables, library location settings etc..)
-
-echo "LD_LIBRARY_PATH=${INSTALL_PATH}/thrift/lib:$LD_LIBRARY_PATH" >> $GITHUB_ENV
