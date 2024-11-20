aeea6c9e93f0c970e166683d7a11e233a8bbf9d0
Barnabás Domozi <wbqpk3@inf.elte.hu>
2024-11-20 11:40:04 +0100

    [PythonTest] Parser test - functions

--- plugins/python/test/sources/functions.py
+++ plugins/python/test/sources/functions.py
@@ -2,3 +2,92 @@ def hello_world():
     print("Hello, world!")
 
 hello_world()
+
+def runner(func, param1, param2):
+    return func(param1,param2)
+
+def mul(a, b):
+    return a * b
+
+def mul2(a, b):
+    return mul(a,b)
+
+def mul3():
+    return mul
+
+mylib = {
+    "multiply": mul
+}
+
+class MyLib:
+    def __init__(self):
+        self.multiply = mul
+
+mylib2 = MyLib()
+
+a = mul(4,8)
+a2 = runner(mul,4,8)
+a3 = mul3()(4,8)
+a4 = mylib["multiply"](4,8)
+a5 = mylib2.multiply(4,8)
+
+print(a)
+print(a2)
+print(a3)
+print(a4)
+print(a5)
+
+print("----------")
+
+print(mul(4,8))
+print(runner(mul,4,8))
+print(mul3()(4,8))
+print(mylib["multiply"](4,8))
+print(mylib2.multiply(4,8))
+
+def sign(a: int, b: str) -> None:
+    pass
+
+def sign2(
+    a: int,
+    b: str)  -> None:
+    pass
+
+def sign3(
+    a: int = 2,
+    b: str = "hi")  -> None:
+    pass
+
+def sign4( # note
+    a: int = 2, # note 2
+    b: str = "hi")  -> None: # note 3
+    pass
+
+from typing import List, Optional
+
+def annotation(a, b) -> None:
+    pass
+
+def annotation2(a,
+    b) -> str:
+    return "abc"
+
+def annotation3(a,
+    b) -> int:
+    return 0
+
+def annotation4(a,
+    b) -> bool:
+    return True
+
+def annotation5(a,
+    b) -> List[str]:
+    return []
+
+def annotation6(a,
+    b) -> Optional[str]:
+    pass
+
+def annotation7(a,
+    b) -> dict[int, bool]:
+    return {}
--- plugins/python/test/src/pythonparsertest.cpp
+++ plugins/python/test/src/pythonparsertest.cpp
@@ -35,12 +35,195 @@ TEST_F(PythonParserTest, FilesAreInDatabase)
   });
 }
 
-TEST_F(PythonParserTest, FunctionDefinitions)
+TEST_F(PythonParserTest, FunctionDefinition)
 {
   _transaction([&, this]() {
     model::PYName pyname;
 
     pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def hello_world()");
     EXPECT_EQ(pyname.is_definition, true);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def runner(func, param1, param2)");
+    EXPECT_EQ(pyname.is_definition, true);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul(a, b)");
+    EXPECT_EQ(pyname.is_definition, true);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul2(a, b)");
+    EXPECT_EQ(pyname.is_definition, true);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul3()");
+    EXPECT_EQ(pyname.is_definition, true);
+  });
+}
+
+TEST_F(PythonParserTest, FunctionType)
+{
+  _transaction([&, this]() {
+    model::PYName pyname;
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def hello_world()");
+    EXPECT_EQ(pyname.type, "function");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def runner(func, param1, param2)");
+    EXPECT_EQ(pyname.type, "function");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul(a, b)");
+    EXPECT_EQ(pyname.type, "function");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul2(a, b)");
+    EXPECT_EQ(pyname.type, "function");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul3()");
+    EXPECT_EQ(pyname.type, "function");
+  });
+}
+
+TEST_F(PythonParserTest, FunctionParamAST)
+{
+  _transaction([&, this]() {
+    model::PYName pyname;
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 6 &&
+                                             odb::query<model::PYName>::column_start == 12);
+    EXPECT_EQ(pyname.type, "astparam");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 6 &&
+                                             odb::query<model::PYName>::column_start == 18);
+    EXPECT_EQ(pyname.type, "astparam");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 6 &&
+                                             odb::query<model::PYName>::column_start == 26);
+    EXPECT_EQ(pyname.type, "astparam");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 9 &&
+                                             odb::query<model::PYName>::column_start == 9);
+    EXPECT_EQ(pyname.type, "astparam");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 9 &&
+                                             odb::query<model::PYName>::column_start == 12);
+    EXPECT_EQ(pyname.type, "astparam");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 12 &&
+                                             odb::query<model::PYName>::column_start == 10);
+    EXPECT_EQ(pyname.type, "astparam");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 12 &&
+                                             odb::query<model::PYName>::column_start == 13);
+    EXPECT_EQ(pyname.type, "astparam");
+  });
+}
+
+TEST_F(PythonParserTest, FunctionSignatureAST)
+{
+  _transaction([&, this]() {
+    model::PYName pyname;
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 48 &&
+                                             odb::query<model::PYName>::type == "function");
+    EXPECT_EQ(pyname.value, "def sign(a: int, b: str) -> None");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 51 &&
+                                             odb::query<model::PYName>::type == "function");
+    EXPECT_EQ(pyname.value, "def sign2(a: int, b: str) -> None");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 56 &&
+                                             odb::query<model::PYName>::type == "function");
+    EXPECT_EQ(pyname.value, "def sign3(a: int, b: str) -> None");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 61 &&
+                                             odb::query<model::PYName>::type == "function");
+    EXPECT_EQ(pyname.value, "def sign4(a: int, b: str) -> None");
+  });
+}
+
+TEST_F(PythonParserTest, FunctionAnnotationAST)
+{
+  _transaction([&, this]() {
+    model::PYName pyname;
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 68 &&
+                                             odb::query<model::PYName>::type == "annotation");
+    EXPECT_EQ(pyname.value, "None");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 72 &&
+                                             odb::query<model::PYName>::type == "annotation");
+    EXPECT_EQ(pyname.value, "str");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 76 &&
+                                             odb::query<model::PYName>::type == "annotation");
+    EXPECT_EQ(pyname.value, "int");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 80 &&
+                                             odb::query<model::PYName>::type == "annotation");
+    EXPECT_EQ(pyname.value, "bool");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 84 &&
+                                             odb::query<model::PYName>::type == "annotation");
+    EXPECT_EQ(pyname.value, "List[str]");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 88 &&
+                                             odb::query<model::PYName>::type == "annotation");
+    EXPECT_EQ(pyname.value, "Optional[str]");
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::line_start == 92 &&
+                                             odb::query<model::PYName>::type == "annotation");
+    EXPECT_EQ(pyname.value, "dict[int, bool]");
+  });
+}
+
+TEST_F(PythonParserTest, FunctionCall)
+{
+  _transaction([&, this]() {
+    model::PYName pyname;
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def hello_world()");
+    EXPECT_EQ(pyname.is_call, false);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def runner(func, param1, param2)");
+    EXPECT_EQ(pyname.is_call, false);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul(a, b)");
+    EXPECT_EQ(pyname.is_call, false);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul2(a, b)");
+    EXPECT_EQ(pyname.is_call, false);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "def mul3()");
+    EXPECT_EQ(pyname.is_call, false);
+
+    // ----------
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "mul" &&
+                                             odb::query<model::PYName>::line_start == 13);
+    EXPECT_EQ(pyname.is_call, true);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "mul" &&
+                                             odb::query<model::PYName>::line_start == 16);
+    EXPECT_EQ(pyname.is_call, false);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "mul" &&
+                                             odb::query<model::PYName>::line_start == 19);
+    EXPECT_EQ(pyname.is_call, false);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "mul" &&
+                                             odb::query<model::PYName>::line_start == 24);
+    EXPECT_EQ(pyname.is_call, false);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "mul" &&
+                                             odb::query<model::PYName>::line_start == 28);
+    EXPECT_EQ(pyname.is_call, true);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "mul" &&
+                                             odb::query<model::PYName>::line_start == 29);
+    EXPECT_EQ(pyname.is_call, false);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "mul" &&
+                                             odb::query<model::PYName>::line_start == 42);
+    EXPECT_EQ(pyname.is_call, true);
+
+    pyname = _db->query_value<model::PYName>(odb::query<model::PYName>::value == "mul" &&
+                                             odb::query<model::PYName>::line_start == 43);
+    EXPECT_EQ(pyname.is_call, false);
   });
 }
