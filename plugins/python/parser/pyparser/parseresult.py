4ddb215cd90c401b2cb4edb38d1bd8f168388684
Barnabás Domozi <barnabas.domozi.ext@ericsson.com>
2025-09-23 15:54:29 +0200

    Do not share list objects across dataclasses
    See: https://docs.python.org/3/library/dataclasses.html#mutable-default-values

--- plugins/python/parser/pyparser/parseresult.py
+++ plugins/python/parser/pyparser/parseresult.py
@@ -1,8 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class ParseResult:
     path: str
     status: str = "full"
-    nodes = []
-    imports = []
+    nodes: list = field(default_factory=list)
+    imports: list = field(default_factory=list)
