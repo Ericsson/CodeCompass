6d6c42ee9b51fbfd65d3caab68e5cdd221d8de34
Máté Cserép <mcserep@gmail.com>
2025-10-12 15:27:11 +0200

    Update .NET dependency to 8.0 and simplify installation.

--- .github/scripts/ubuntu-22.04/setup_build.sh
+++ .github/scripts/ubuntu-22.04/setup_build.sh
@@ -6,11 +6,5 @@ sudo apt-get install --yes \
   llvm-15-dev clang-15 libclang-15-dev \
   gcc-11-plugin-dev thrift-compiler libthrift-dev \
   default-jdk libssl-dev libgraphviz-dev libmagic-dev libgit2-dev exuberant-ctags doxygen \
-  libldap2-dev libgtest-dev
-
-wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-6.0
+  libldap2-dev libgtest-dev \
+  dotnet-sdk-8.0
