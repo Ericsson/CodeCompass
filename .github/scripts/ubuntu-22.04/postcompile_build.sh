c86197aa6f6a1a60908ab40791c25c78d4f18b82
Máté Cserép <mcserep@gmail.com>
2025-12-09 07:15:09 +0100

    Clean up build2-toolchain intermediate binaries
    Remove build2-toolchain intermediate binaries during cleanup.

--- .github/scripts/ubuntu-22.04/postcompile_build.sh
+++ .github/scripts/ubuntu-22.04/postcompile_build.sh
@@ -8,3 +8,4 @@ echo "CMAKE_PREFIX_PATH=${INSTALL_PATH}/odb:$CMAKE_PREFIX_PATH" >> $GITHUB_ENV
 # Clean up dependency sources and intermediate binaries to save space
 rm -rf ${DOWNLOAD_PATH}/odb
 rm -rf ${INSTALL_PATH}/build2
+rm -rf ./build2-toolchain-*
