15ffd103d71bd28bb929c142338056ecc5ceca3e
Nani0002 <molnardani002@gmail.com>
2025-12-18 14:38:10 +0100

    Fix CodeQL string escape issue

--- webgui-new/scripts/patch-basepath.js
+++ webgui-new/scripts/patch-basepath.js
@@ -5,11 +5,15 @@ const NEW_BASE = process.env.APP_BASE_PATH || '/new';
 
 console.log(`[patch-basepath] ${PLACEHOLDER} -> ${NEW_BASE}`);
 
+function escapeRegExp(s) {
+  return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
 try {
   const results = replaceInFileSync({
     files: ['out/**/*.html', 'out/**/*.js', 'out/**/*.json'],
-    from: new RegExp(PLACEHOLDER.replace(/\//g, '\\/'), 'g'),
-    to: NEW_BASE,
+    from: new RegExp(escapeRegExp(PLACEHOLDER), 'g'),
+    to: () => NEW_BASE,
   });
   console.log('[patch-basepath] modified files:', results.length);
 } catch (err) {
